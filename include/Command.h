5405f24a13cdb118809cb85209f378a418884110
Alaa Brahim <alaa.brahim@etudiant-fst.utm.tn>
2025-01-13 19:22:52 +0100

    Enhance Command class to include CommandSource for improved command issuer handling

--- include/Command.h
+++ include/Command.h
@@ -3,21 +3,33 @@
 
 #include "Arduino.h"
 
+enum CommandSource
+{
+    SERIAL_COMMAND,
+    TELEGRAM_COMMAND,
+    ALEXA_COMMAND
+};
+
 class Command
 {
 public:
     Command();
-    Command(String command, String issuer);
+    Command(String command, CommandSource source, String issuer);
+    Command(String command, CommandSource source);
     bool isCommandIssuerAdmin();
     bool isCommandIssuerNormalUser();
     String getCommand() { return command; }
     String getIssuer() { return issuer; }
+    CommandSource getSource() { return source; }
     void setCommand(String command);
+    void setSource(CommandSource CommandSource);
     void setIssuer(String issuer);
 
 private:
     String command = "";
     String issuer = "";
+    CommandSource source;
+    String cleanString(String str);
 };
 
 #endif
--- src/Command.cpp
+++ src/Command.cpp
@@ -3,21 +3,30 @@
 
 Command::Command() {}
 
-Command::Command(String command, String issuer)
+Command::Command(String command, CommandSource source, String issuer)
 {
-    command.toLowerCase();
-    command.trim();
-
-    issuer.toLowerCase();
-    issuer.trim();
+    command = cleanString(command);
+    issuer = cleanString(issuer);
 
     this->command = command;
+    this->source = source;
     this->issuer = issuer;
+}
+
+Command::Command(String command, CommandSource source)
+{
+    command = cleanString(command);
+
+    this->command = command;
+    this->source = source;
 }
 
 bool Command::isCommandIssuerAdmin()
 {
-    if (this->issuer == "serial" || UserManager::isAdmin(this->issuer))
+    if (this->source == CommandSource::SERIAL_COMMAND)
+        return true;
+
+    if (this->source == CommandSource::TELEGRAM_COMMAND && UserManager::isAdmin(this->issuer))
         return true;
 
     return false;
@@ -25,7 +34,10 @@ bool Command::isCommandIssuerAdmin()
 
 bool Command::isCommandIssuerNormalUser()
 {
-    if (this->issuer == "alexa" || UserManager::isUser(this->issuer))
+    if (this->source == CommandSource::ALEXA_COMMAND)
+        return true;
+
+    if (this->source == CommandSource::TELEGRAM_COMMAND && UserManager::isUser(this->issuer))
         return true;
 
     return false;
@@ -33,14 +45,22 @@ bool Command::isCommandIssuerNormalUser()
 
 void Command::setCommand(String command)
 {
-    command.toLowerCase();
-    command.trim();
-    this->command = command;
+    this->command = cleanString(command);
+}
+
+void Command::setSource(CommandSource source)
+{
+    this->source = source;
 }
 
 void Command::setIssuer(String issuer)
 {
-    issuer.toLowerCase();
-    issuer.trim();
-    this->issuer = issuer;
+    this->issuer = cleanString(issuer);
+}
+
+String Command::cleanString(String str)
+{
+    str.toLowerCase();
+    str.trim();
+    return str;
 }
